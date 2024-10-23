da8c769219a9de0b239f93c915871d0d92ac7531
이상용 <lsy3709@naver.com>
2024-10-23 16:51:23 +0900

    UI 전체 확인

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+
 import 'package:calendar_scheduler_lsy/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: HomeScreen(),
-  ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting();
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
