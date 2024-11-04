43da26126ff325014fcf316d2e3edf1464e0e275
이상용 <lsy3709@naver.com>
2024-11-04 12:25:48 +0900

    16장 확인 후 커밋 정리

---
+++ lib/const/api.dart
@@ -0,0 +1,4 @@
+// const API_KEY = '여기에 키를 입력해주세요!!';
+const API_KEY = '여기에 키를 입력해주세요!!';
+const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/search'; // Youtube Data API V3 URL
+const CF_CHANNEL_ID = 'UCMnBfVB2S1woKCwQEAFN-rQ'; // 각자 유튜브 채널 ID
