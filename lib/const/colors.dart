813e4adeb6f828fbf01b4f45dceeec691ed16cc7
이상용 <lsy3709@naver.com>
2024-11-04 17:43:39 +0900

    11장 확인 및 커밋 정리

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -6,3 +6,6 @@ final DARK_GREY_COLOR = Colors.grey[600]!;
 final TEXT_FIELD_FILL_COLOR = Colors.grey[300]!;
 const SECONDARY_COLOR = Color(0xFF335CB0);
 const ERROR_COLOR = Colors.red;
+const backgroundColor = Color(0xFF0E0E0E);  // 배경색
+const primaryColor = Colors.white;  // 주 색상
+final secondaryColor = Colors.grey[600];  // 보조 색상
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
-import 'package:calendar_scheduler_lsy/screen/home_screen.dart';
+import 'package:calendar_scheduler_lsy/screen/root_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:calendar_scheduler_lsy/screen/home_screen.dart';
+import 'package:calendar_scheduler_lsy/const/colors.dart';
+import 'package:calendar_scheduler_lsy/screen/root_screen.dart';
 
+import 'const/colors.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        sliderTheme: SliderThemeData(  // Slider 위젯 관련
+          thumbColor: primaryColor,    // 동그라미 색
+          activeTrackColor: primaryColor,  // 이동한 트랙 색
+
+          // 아직 이동하지 않은 트랙 색
+          inactiveTrackColor: primaryColor.withOpacity(0.3),         ),
+        // BottomNavigationBar 위젯 관련
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: primaryColor,     // 선택 상태 색
+          unselectedItemColor: secondaryColor, // 비선택 상태 색
+          backgroundColor: backgroundColor,    // 배경 색
+        ),
+      ),
+      home: RootScreen(),
     ),
   );
 }
