8092bd3e818a8b2ea86ed862d2a8fba3cc738436
이상용 <lsy3709@naver.com>
2024-11-04 11:11:02 +0900

    23장 슈파베이스 로그 아웃시, 구글 계정 같이 로그아웃 부분 수정 확인

--- lib/component/today_banner.dart
+++ lib/component/today_banner.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../const/colors.dart';
 
 class TodayBanner extends StatelessWidget {
   final DateTime selectedDate; // ➊ 선택된 날짜
   final int count; // ➋ 일정 개수
+
 
   const TodayBanner({
     required this.selectedDate,
@@ -15,6 +17,9 @@ class TodayBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // GoogleSignIn 인스턴스 생성
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
     final textStyle = TextStyle(
       // 기본으로 사용할 글꼴
       fontWeight: FontWeight.w600,
@@ -43,8 +48,9 @@ class TodayBanner extends StatelessWidget {
               children: [
                 GestureDetector(
                   onTap: () async {
-                    await Supabase.instance.client.auth.signOut();
-
+                    await Supabase.instance.client.auth.signOut(scope: SignOutScope.global);
+                    // Google 로그아웃
+                    await googleSignIn.signOut();
                     Navigator.of(context).pop();
                   },
                   child: Icon(
