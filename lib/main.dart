61e93b3e8dbcade93906d9dac2ba4cf89cbc0db6
이상용 <lsy3709@naver.com>
2024-11-05 12:10:08 +0900

    7장 확인 및 커밋 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,41 @@
-import 'package:calendar_scheduler_lsy/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // 플러터 프레임워크가 앱을 실행할 준비가 될때까지
-  // 기다림
-  WidgetsFlutterBinding.ensureInitialized();
-
   runApp(
-    MaterialApp(
-      home: HomeScreen(),
-    ),
+    SplashScreen(),
   );
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFF99231),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'asset/img/logo.png',
+                    width: 200,
+                  ),
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                      Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
